ac53fae4e868d58ebcfb343149a9176fce8afdd9
AnissaLopez <anissa965@gmail.com>
2023-03-23 18:58:51 -0400

    this was harder than I anticipated

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+let arrayNames = ['Anissa', 'Paul', 'Camila', 'Chase'];
+
+function writeCards(array, event){
+ let newArray = []
+ for (let i =0; i<array.length; i++){
+     newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+ }
+     
+     return newArray;
+ }
+
+
+
+writeCards(arrayNames, 'Birthday')
+//for (let i = 0; i<10; i++){
+
+
+
+function countDown(num){
+    let i = 0;
+    while(i<=10){
+      console.log(num--);
+      i++
+    }
+    return num;
+   }
+   
+   countDown(10)
