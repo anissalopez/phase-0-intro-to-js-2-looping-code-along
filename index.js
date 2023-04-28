438ad0d291daeebd915edfe696c7ef64585d80a4
anissalopez <anissa965@gmail.com>
2023-04-28 15:22:35 -0400

    looping

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 // Code your solutions in this file
 let arrayNames = ['Anissa', 'Paul', 'Camila', 'Chase'];
 
-function writeCards(array, event){
- let newArray = []
- for (let i =0; i<array.length; i++){
-     newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
- }
-     
-     return newArray;
- }
 
 
-
-writeCards(arrayNames, 'Birthday')
-//for (let i = 0; i<10; i++){
-
-
+const writeCards = (array, occasion) => {
+    let message = [];
+    for (let i = 0; i < array.length; i++){  
+        message.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return message;
+}
 
 function countDown(num){
-    let i = 0;
-    while(i<=10){
-      console.log(num--);
-      i++
+    while (num>=0){
+      console.log(num--)
+    }  
     }
-    return num;
-   }
-   
-   countDown(10)
